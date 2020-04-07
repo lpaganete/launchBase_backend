55ab868e871f7572dc823770b3b85a862a7f266c
Luís Paulo Aganete Reis <luispaulo.aganete@hotmail.com>
2020-04-07 17:44:26 -0300

    Instalando dependência nodemon

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 const express = require('express')
+
+const server = express()
+
+
+ //2-Configurando a rota da pagina index (about)
+ server.get("/", function(rec, res) { 
+    return res.render("index") //chamando a antiga pagina index
+})
+
+
+
+//1-setando a porta que o servidor vai rodar e exibindo mensagem caso o ele execute
+server.listen(5001, function() {
+    console.log("Server is running!")
+})git shortlog
