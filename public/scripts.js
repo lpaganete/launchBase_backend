5e32db78ee02dbc7ae9bae20f8f8fb43f1246f94
Luís Paulo Aganete Reis <luispaulo.aganete@hotmail.com>
2020-04-09 13:30:46 -0300

    Abrindo página com video quando clico em cada card

--- public/scripts.js
+++ public/scripts.js
@@ -13,27 +13,8 @@ for (let card of cards) {
         
         const videoId = card.getAttribute("id") //! getAttribute, permite pegar qualquer atributo dentro da div 
 
-        modalOverlay.classList.add('active')   //quando clicar em algum card, ele vai adicionar a classe modalOverlay o active. que abre o modal atrvés do CSS.
-
-        modalOverlay.querySelector("iframe").src = `https://www.youtube.com/embed/${videoId }` //! definindo os videos para cada grid. Ao inserir as linhas com comentári em vermelho. E definindo o id de cada url de  vídeo como id de cada div, eu consigo armazena-lo em uma variavel, e depois modificalo de acordo com cada grid.
+        //Dizendo para que página que eu quero que vá quando clicar no card
+        window.location.href = `/video?id=${videoId}`  
  
      }) 
 } 
-
-/*FECHANDO O MODAL*/
-
-const closeModal = document.querySelector('.close-modal').addEventListener("click", function (){
-    modalOverlay.classList.remove("active") //classlist ele vai pegar todos os elementos que estiverem dentro do  <div class="modal-overlay">  
-
-    modalOverlay.querySelector("iframe").src = ""; //! Quando fechar o modal o video para de reproduzir. Posis está retornando url vazia
-})  
-
-/*Maximizando Modal
-(dica: utilize a mesma lógica implementada para fechar o modalOverlay, mas trabalhe com a classe modal e utilize o método contains do classList para verificar se o elemento está ou não com a classe maximize).
-*/
-
-const maximizeModal = document.querySelector('.modal').addEventListener("click", function (){
-    modalOverlay.classList.contains("maximize") //classlist ele vai pegar todos os elementos que estiverem dentro do  <div class="modal-overlay">  
-   
-})  
-
